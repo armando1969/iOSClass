caa59f4a57dd4815405aaaf1aa3d65e4aee2ac29
Consultant <consultant@AdminisatorsMBP.hsd1.ga.comcast.net>
2022-01-24 17:15:40 -0500

    Made changes to the Homework

--- Week1/Day2/Closures.playground/Contents.swift
+++ Week1/Day2/Closures.playground/Contents.swift
@@ -9,8 +9,20 @@ let name: String = "Armando"
 
 // Closure
 
-let closure: () -> Void = {
+var closure: () -> Void = {
     print("My Closure")
 }
 
 closure()
+
+closure = {
+    print("another message")
+}
+typealias Closure = (Int) -> Void
+
+func fetchFromApi(from url: String, completion: Closure) {
+    let numFromAPI = 200
+    completion(numFromAPI)
+}
+
+fetchFromApi(from: "https://api.themoviedb.org", completion: { codeAny in  print(codeAny)}) 
